94bf98ee58055e78fa8f1a3c8e25ee81aa1fc913
Robin Lemaire <robin.lemaire@adevinta.com>
2025-07-28 15:00:58 +0200

    [Fix] linter

--- Tests/UnitTests/Model/Internal/Colors/SwitchDynamicColorsTests.swift
+++ Tests/UnitTests/Model/Internal/Colors/SwitchDynamicColorsTests.swift
@@ -1,43 +1,23 @@
+//
+//  ToggleDynamicColorsTests.swift
+//  SparkSelectionControlsSnapshotTests
+//
+//  Created by robin.lemaire on 28/07/2025.
+//  Copyright © 2025 Leboncoin. All rights reserved.
+//
+
 import XCTest
-@_spi(SI_SPI) @testable import SparkSwitch
+@testable import SparkSwitch
+@_spi(SI_SPI) @testable import SparkTheming
 
-final class SwitchDynamicColorsTests: XCTestCase {
+final class ToggleDynamicColorsTests: XCTestCase {
+
+    // MARK: - Tests
+
     func test_defaultValues() {
-        let colors = SwitchDynamicColors()
-        XCTAssertTrue(colors.backgroundColors.equals(ColorTokenClear()))
-        XCTAssertTrue(colors.dotForegroundColors.equals(ColorTokenClear()))
-    }
-
-    func test_propertyAssignment() {
-        let mockColor1 = ColorTokenMock(id: "bg")
-        let mockColor2 = ColorTokenMock(id: "dot")
-        let colors = SwitchDynamicColors(backgroundColors: mockColor1, dotForegroundColors: mockColor2)
-        XCTAssertTrue(colors.backgroundColors.equals(mockColor1))
-        XCTAssertTrue(colors.dotForegroundColors.equals(mockColor2))
-    }
-
-    func test_equatable_true_whenAllPropertiesEqual() {
-        let mockColor1 = ColorTokenMock(id: "bg")
-        let mockColor2 = ColorTokenMock(id: "dot")
-        let lhs = SwitchDynamicColors(backgroundColors: mockColor1, dotForegroundColors: mockColor2)
-        let rhs = SwitchDynamicColors(backgroundColors: mockColor1, dotForegroundColors: mockColor2)
-        XCTAssertEqual(lhs, rhs)
-    }
-
-    func test_equatable_false_whenPropertiesDiffer() {
-        let mockColor1 = ColorTokenMock(id: "bg")
-        let mockColor2 = ColorTokenMock(id: "dot")
-        let mockColor3 = ColorTokenMock(id: "other")
-        let lhs = SwitchDynamicColors(backgroundColors: mockColor1, dotForegroundColors: mockColor2)
-        let rhs = SwitchDynamicColors(backgroundColors: mockColor3, dotForegroundColors: mockColor2)
-        XCTAssertNotEqual(lhs, rhs)
+        let colors = SwitchStaticColors()
+        XCTAssertTrue(colors.dotBackgroundColor.equals(ColorTokenClear()))
+        XCTAssertTrue(colors.textForegroundColor.equals(ColorTokenClear()))
+        XCTAssertTrue(colors.hoverColor.equals(ColorTokenClear()))
     }
 }
-
-// MARK: - Mock
-private struct ColorTokenMock: ColorToken, Equatable {
-    let id: String
-    func equals(_ other: any ColorToken) -> Bool {
-        (other as? ColorTokenMock)?.id == id
-    }
-} 
--- Tests/UnitTests/Model/Internal/Colors/SwitchStaticColorsTests.swift
+++ Tests/UnitTests/Model/Internal/Colors/SwitchStaticColorsTests.swift
@@ -1,48 +1,23 @@
-import XCTest
-@_spi(SI_SPI) @testable import SparkSwitch
+//
+//  ToggleStaticColorsTests.swift
+//  SparkSelectionControlsSnapshotTests
+//
+//  Created by robin.lemaire on 28/07/2025.
+//  Copyright © 2025 Leboncoin. All rights reserved.
+//
 
-final class SwitchStaticColorsTests: XCTestCase {
+import XCTest
+@testable import SparkSwitch
+@_spi(SI_SPI) @testable import SparkTheming
+
+final class ToggleStaticColorsTests: XCTestCase {
+
+    // MARK: - Tests
+
     func test_defaultValues() {
         let colors = SwitchStaticColors()
         XCTAssertTrue(colors.dotBackgroundColor.equals(ColorTokenClear()))
         XCTAssertTrue(colors.textForegroundColor.equals(ColorTokenClear()))
         XCTAssertTrue(colors.hoverColor.equals(ColorTokenClear()))
     }
-
-    func test_propertyAssignment() {
-        let mockColor1 = ColorTokenMock(id: "dot")
-        let mockColor2 = ColorTokenMock(id: "text")
-        let mockColor3 = ColorTokenMock(id: "hover")
-        let colors = SwitchStaticColors(dotBackgroundColor: mockColor1, textForegroundColor: mockColor2, hoverColor: mockColor3)
-        XCTAssertTrue(colors.dotBackgroundColor.equals(mockColor1))
-        XCTAssertTrue(colors.textForegroundColor.equals(mockColor2))
-        XCTAssertTrue(colors.hoverColor.equals(mockColor3))
-    }
-
-    func test_equatable_true_whenAllPropertiesEqual() {
-        let mockColor1 = ColorTokenMock(id: "dot")
-        let mockColor2 = ColorTokenMock(id: "text")
-        let mockColor3 = ColorTokenMock(id: "hover")
-        let lhs = SwitchStaticColors(dotBackgroundColor: mockColor1, textForegroundColor: mockColor2, hoverColor: mockColor3)
-        let rhs = SwitchStaticColors(dotBackgroundColor: mockColor1, textForegroundColor: mockColor2, hoverColor: mockColor3)
-        XCTAssertEqual(lhs, rhs)
-    }
-
-    func test_equatable_false_whenPropertiesDiffer() {
-        let mockColor1 = ColorTokenMock(id: "dot")
-        let mockColor2 = ColorTokenMock(id: "text")
-        let mockColor3 = ColorTokenMock(id: "hover")
-        let mockColor4 = ColorTokenMock(id: "other")
-        let lhs = SwitchStaticColors(dotBackgroundColor: mockColor1, textForegroundColor: mockColor2, hoverColor: mockColor3)
-        let rhs = SwitchStaticColors(dotBackgroundColor: mockColor4, textForegroundColor: mockColor2, hoverColor: mockColor3)
-        XCTAssertNotEqual(lhs, rhs)
-    }
 }
-
-// MARK: - Mock
-private struct ColorTokenMock: ColorToken, Equatable {
-    let id: String
-    func equals(_ other: any ColorToken) -> Bool {
-        (other as? ColorTokenMock)?.id == id
-    }
-} 
